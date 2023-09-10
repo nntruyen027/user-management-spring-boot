53d7d1bb903ff74d2c284d21bb71e414b61a4afd
Nguyen Ngoc Truyen <nntruyen027@gmail.com>
2023-09-10 17:26:29 +0700

    khởi tạo dự án

---
+++ App/src/test/java/app/user/demo/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package app.user.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
